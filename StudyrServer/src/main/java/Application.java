586a649a60836988282b619c76af2d112e691cc4
MtuMartian <mrmartin@mtu.edu>
2016-02-26 16:05:33 -0500

    Connected server to local redis database. POST requests do not work for unknown reasons.

--- StudyrServer/src/main/java/Application.java
+++ StudyrServer/src/main/java/Application.java
@@ -1,11 +1,33 @@
 /**
  * Created by miles on 2/15/16.
  */
+import redis.clients.jedis.Jedis;
+
 import static spark.Spark.*;
 public class Application {
 
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello, World!");
+
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("foo", "bar");
+
+        get("/hello", (req, res) -> "Hello, World!");
+
+        get("/redis", (req, res) -> jedis.get("foo"));
+
+        post("/redis/:key/post", (req, res) -> {
+            String key = req.params("key");
+//            String value = req.queryParams("value");
+
+            jedis.set(key, "martin");
+
+
+            return "martin";
+        });
+
+        get("/redis/:key/get", (req, res) ->
+            jedis.get(req.params(":key")));
+
     }
 
 }
---
+++ StudyrServer/src/main/java/Data/User.java
@@ -0,0 +1,14 @@
+package Data;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by miles on 2/16/16.
+ */
+public class User {
+    private UUID userID;
+    private String name;
+    private double gpa;
+    private List<String> subjects;
+}
